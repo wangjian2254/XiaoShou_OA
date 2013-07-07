abdd9aaccdc9e8eca46ebb177bbb4bc2776d25ae
王健 <wangjian2254@gmail.com>
2013-07-08 06:35:35 +0800

    员工录入

--- xiaoshou_oa/models.py
+++ xiaoshou_oa/models.py
@@ -1,3 +1,10 @@
+#code=utf-8
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    user=models.OneToOneField(User)
+    sex=models.BooleanField(default=True, verbose_name=u'性别', help_text=u'性别')
+
