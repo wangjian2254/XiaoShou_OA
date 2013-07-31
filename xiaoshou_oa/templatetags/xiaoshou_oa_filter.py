f2c7fc31e31c07b621de2e4d8c706f1c9d019b80
王健 <wangjian2254@gmail.com>
2013-08-01 07:24:53 +0800

    二维码 优化

--- xiaoshou_oa/templatetags/xiaoshou_oa_filter.py
+++ xiaoshou_oa/templatetags/xiaoshou_oa_filter.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 import urllib
+from xiaoshou_oa.models import ProductType
 
 __author__ = '王健'
 
@@ -10,6 +11,7 @@ register=template.Library()
 @register.filter(name='erweima')
 def erweima(adminform,object_name):
     flag=True
+    isModel=False
     s=u"{"
     for fieldset in adminform:
         for line in fieldset:
@@ -23,12 +25,23 @@ def erweima(adminform,object_name):
                         s+=u'"%s":"%s",'%(field.field.name,adminform.form.instance)
                         if field.field.value():
                             flag=False
+                            isModel=True
+
                     except:
                         pass
                 elif field.field.name=='name':
                     s+=u'"%s":"%s",'%(field.field.name,field.field.value())
                     if field.field.value():
                         flag=False
+
+    if isModel:
+        slist=[]
+        for type in ProductType.objects.filter(isdel=False):
+            slist.append(s+u'"type":"%s","typename":"%s","class":"%s"}'%(type.flag,type.name,object_name))
+        erweistr=u''
+        for er in slist:
+            erweistr+=u'''<a href="http://qr.liantu.com/api.php?text=%s"><img src="http://qr.liantu.com/api.php?text=%s" border="0"/></a><a href="https://chart.googleapis.com/chart?cht=qr&chs=300x300&choe=UTF-8&chld=L|2&chl=%s"><img src="https://chart.googleapis.com/chart?cht=qr&chs=300x300&choe=UTF-8&chld=L|2&chl=%s" border="0"/></a>'''%(urllib.quote(er.encode('utf-8')),urllib.quote(er.encode('utf-8')),urllib.quote(er.encode('utf-8')),urllib.quote(er.encode('utf-8')))
+        return erweistr
     s+=u'"class":"%s"}'%object_name
     if not flag:
         return u'''<a href="http://qr.liantu.com/api.php?text=%s"><img src="http://qr.liantu.com/api.php?text=%s" border="0"/></a><a href="https://chart.googleapis.com/chart?cht=qr&chs=300x300&choe=UTF-8&chld=L|2&chl=%s"><img src="https://chart.googleapis.com/chart?cht=qr&chs=300x300&choe=UTF-8&chld=L|2&chl=%s" border="0"/></a>'''%(urllib.quote(s.encode('utf-8')),urllib.quote(s.encode('utf-8')),urllib.quote(s.encode('utf-8')),urllib.quote(s.encode('utf-8')))
